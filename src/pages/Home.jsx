48ea187a9875a478f9cc16c3b9bfd89dac7dcf69
RomanKozlovsky <sendmemessagenow@gmail.com>
2024-01-08 16:55:29 +0200

    add finaly style to categories items

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,24 @@
 import style from "./Home.module.css";
 import banner from "../assets/img/banner.png";
-import clothes from '../assets/img/clothes.jpg';
-import libkos from '../assets/img/libkos.jpg';
-import other from '../assets/img/other.jpg'
+import clothes from "../assets/img/clothes.jpg";
+import libkos from "../assets/img/libkos.jpg";
+import other from "../assets/img/other.jpg";
 
 export default function Home() {
   return (
     <>
       <div className={style.home_wrapper}>
-        <img className={banner_home} src={banner} alt="banner_wanderlust" />
+        <img className={style.banner_home} src={banner} alt="banner_wanderlust" />
         <div className={style.categories}>
-          <ul className={style.categories_list}>
-            <li className={style.categories_items}><img src={clothes} alt="" /></li>
-            <li className={style.categories_items}><img src={libkos} alt="" /></li>
-            <li className={style.categories_items}><img src={other} alt="" /></li>
-          </ul>
+          <div className={style.categories_item_clothes}>
+            <p>CLOTHES</p>
+          </div>
+          <div className={style.categories_item_libkos}>
+            <p>LIBKOS</p>
+          </div>
+          <div className={style.categories_item_other}>
+            <p>OTHERS</p>
+          </div>
         </div>
       </div>
     </>
