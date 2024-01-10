1872767270ed61ecf889ab2760f7fabc0a72d8aa
RomanKozlovsky <sendmemessagenow@gmail.com>
2024-01-10 18:47:25 +0200

    add context to project

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Clothes from "./pages/Clothes";
+import React from "react";
+
+export const Context = React.createContext();
 
 export default function App() {
+  const [testData, setTestData] = React.useState("hellow, i'm context");
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<Home />} />
-          <Route path="clothes" element={<Clothes />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <Context.Provider value={{ testData, setTestData }}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="" element={<Home />} />
+            <Route path="clothes" element={<Clothes />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Context.Provider>
     </div>
   );
 }
--- src/pages/Clothes.jsx
+++ src/pages/Clothes.jsx
@@ -1,16 +1,19 @@
+import { Context } from "../App";
 import style from "./Clothes.module.css";
 import React, { useEffect } from "react";
 
 export default function Clothes() {
   const [testState, setTestState] = React.useState([]);
 
+  const { testData, setTestData } = React.useContext(Context); //отримую дані з app.js на пряму, все працює
+
   useEffect(() => {
+    console.log(testData);
     fetch("https://659a8ae0652b843dea53af1f.mockapi.io/items")
       .then((response) => response.json())
       .then((data) => setTestState(data));
   }, []);
 
-  console.log(testState);
   return (
     <>
       <div className={style.wrapper_clothes}>
@@ -19,9 +22,6 @@ export default function Clothes() {
             <div className={style.clothes_item}>
               <img src={item.imageUrl} alt="image clothes" />
               <p>{item.title.toUpperCase()}</p>
-              {/* <p>
-                {item.size[0]}, {item.size[1]}, {item.size[2]}, {item.size[3]}
-              </p> */}
               <p>
                 <b>UAH {item.price}</b>
               </p>
