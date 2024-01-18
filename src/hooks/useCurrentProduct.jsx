916e9e49ad596cc1a949be54ca73457042ad70a3
RomanKozlovsky <sendmemessagenow@gmail.com>
2024-01-18 21:17:38 +0200

    add currentProduct hooks and create context

--- src/hooks/useCurrentProduct.jsx
+++ src/hooks/useCurrentProduct.jsx
@@ -1,16 +1,7 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React from "react";
 
 export default function useCurrentProduct() {
-  const [currentProduct, setCurrentProduct] = React.useState({});
+  const [currentProduct, setCurrentProduct] = React.useState([]);
 
-  let params = useParams();
-
-  useEffect(() => {
-    fetch(`https://659a8ae0652b843dea53af1f.mockapi.io/items/${params.id}`)
-      .then((response) => response.json())
-      .then((response) => setCurrentProduct(response));
-  }, [params.id]);
-
-  return currentProduct;
+  return { currentProduct, setCurrentProduct };
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,48 +1,46 @@
 import React, { useEffect } from "react";
-// import { CurrentPrice, CurrentSize } from "../App";
-// import style from "./Product.module.css";
-// import { useParams } from "react-router-dom";
+import { CurrentPrice } from "../App";
+import style from "./Product.module.css";
+import { useParams } from "react-router-dom";
 import { useStore } from "../contexts/GlobalContext";
 
 export default function Product() {
   const { currentProduct } = useStore();
 
-  // const { currentPrice } = React.useContext(CurrentPrice);
-  // const { setCurrentSize } = React.useContext(CurrentSize);
-  // // const [currentProduct, setCurrentProduct] = React.useState([]);
-  // const [countProduct, setCountProduct] = React.useState(1);
-  // const currentImg = currentProduct.imageUrl;
+  const { currentPrice } = React.useContext(CurrentPrice);
+  const [countProduct, setCountProduct] = React.useState(1);
+  const currentImg = currentProduct.currentProduct.imageUrl;
 
-  // function selectSize(id) {
-  //   setCurrentSize(id);
-  // }
+  let params = useParams();
 
-  // let params = useParams();
+  useEffect(() => {
+    fetch(`https://659a8ae0652b843dea53af1f.mockapi.io/items/${params.id}`)
+      .then((response) => response.json())
+      .then((response) => currentProduct.setCurrentProduct(response));
+  }, [params.id]);
 
-  // useEffect(() => {
-  //   fetch(`https://659a8ae0652b843dea53af1f.mockapi.io/items/${params.id}`)
-  //     .then((response) => response.json())
-  //     .then((response) => setCurrentProduct(response));
-  // }, [params.id]);
-
-  if (!currentProduct) {
+  if (!currentProduct.currentProduct) {
     return "loading...";
   }
 
   return (
     <>
-      {/* <div className={style.product_wrapper}>
+      <div className={style.product_wrapper}>
         <div className={style.product_item}>
-          <img src={currentImg} alt={`${currentProduct.title}`} />
+          <img src={currentImg} alt={`${currentProduct.currentProduct.title}`} />
           <div className={style.product_description}>
-            <h1>{currentProduct.title}</h1>
-            {currentProduct.size?.map((item) => (
-              <span onClick={() => selectSize(item)} key={item} className={style.product_size}>
+            <h1>{currentProduct.currentProduct.title}</h1>
+            {currentProduct.currentProduct.size?.map((item) => (
+              <span key={item} className={style.product_size}>
                 {item}
               </span>
             ))}
             <p>
-              {currentPrice === "UAH" ? <b>{currentProduct.price * countProduct} UAH</b> : <b>{((currentProduct.price / 38) * countProduct).toFixed(2)} USD</b>}
+              {currentPrice === "UAH" ? (
+                <b>{currentProduct.currentProduct.price * countProduct} UAH</b>
+              ) : (
+                <b>{((currentProduct.currentProduct.price / 38) * countProduct).toFixed(2)} USD</b>
+              )}
             </p>
             <div className={style.countProductBlock}>
               <div onClick={() => setCountProduct(countProduct - 1)} className={style.countProduct_btn}>
@@ -56,7 +54,7 @@ export default function Product() {
             <button className={style.addToCart}>Add to cart</button>
           </div>
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
